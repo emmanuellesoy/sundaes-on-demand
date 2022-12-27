55bac2a2a3039b624a424bd54a056ca48cddfdb3
Emmanuelle Laguna <emmanuelle.laguna@gmail.com>
2022-12-27 01:34:52 -0600

    feature: Mock Service Worker

---
+++ src/mocks/handlers.js
@@ -0,0 +1,16 @@
+import { response, rest } from "msw";
+
+export const handlers = [
+  rest.get("http://localhost3030/scoops", (request, resolve, context) => {
+    return response(
+      context.json([
+        {
+          name: "Chocolate",
+          image: "/images/chocolate.png",
+          name: "Vanilla",
+          image: "/images/vanilla.png",
+        },
+      ])
+    );
+  }),
+];
